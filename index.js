2a77417c039e4908ceaec34b8b66e77df3023241
Christina Bohn <christinajbohn@gmail.com>
2022-01-14 23:34:41 -0800

    Wrote questions for prompts in index.js

--- index.js
+++ index.js
@@ -1,4 +1,85 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
 const employees = [];
+
+const managerQuestions = [
+    {
+        type: 'input',
+        name: 'managerName',
+        message: 'What is the name of the team manager?'
+    },
+    {
+        type: 'input',
+        name: 'managerId',
+        message: 'What is the manager\'s id?'
+    },
+    {
+        type: 'input',
+        name: 'managerEmail',
+        message: 'What is the manager\'s email?'
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'What is the manager\'s office number?'
+    }
+];
+
+const engineerQuestions = [
+    {
+        type: 'input',
+        name: 'engineerName',
+        message: 'What is the name of the engineer?'
+    },
+    {
+        type: 'input',
+        name: 'engineerId',
+        message: 'What is the engineer\'s id?'
+    },
+    {
+        type: 'input',
+        name: 'engineerEmail',
+        message: 'What is the engineer\'s email?'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is the URL for the engineer\'s GitHub?'
+    }
+];
+
+const internQuestions = [
+    {
+        type: 'input',
+        name: 'internName',
+        message: 'What is the name of the intern?'
+    },
+    {
+        type: 'input',
+        name: 'internId',
+        message: 'What is the intern\'s id?'
+    },
+    {
+        type: 'input',
+        name: 'internEmail',
+        message: 'What is the intern\'s email?'
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: 'Which school does the intern attend?'
+    }
+];
+
+const nextQuestion = [
+    {
+        type: 'list',
+        name: 'nextQuestion',
+        message: 'What type of team member would you like to add?',
+        choices: ['Engineer', 'Intern', 'I am done adding team members']
+    }
+]
 
 // Actions needed:
 
