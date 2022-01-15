550de0b62c22cbd35c66132bc9bb6bd665ed0ab2
Christina Bohn <christinajbohn@gmail.com>
2022-01-14 22:51:45 -0800

    Added template style.css file, began pseudocoding for functions to gather data.

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
+// Actions needed:
+
+// Ask user for manager info
+
+    // Prompt user for the data
+
+        // THEN create and store an object for the Manager
+
+        // THEN Ask user what they would like to do next
+
+// Ask user for engineer info
+
+    // Prompt user for the data
+
+        // THEN create and store an object for the Engineer
+
+        // THEN Ask user what they would like to do next
+
+// Ask user for intern info
+
+    // Prompt user for the data
+
+        // THEN create and store an object for the Intern
+
+        // THEN Ask user what they would like to do next
+
+// Ask user what they would like to do next
+
+    // Add Engineer, Add Intern, or Be done
+
+        // IF 'Add Engineer' -> Ask user for engineer info
+
+        // IF 'Add Intern' -> Ask user for intern info
+
+        // IF 'Be done' -> Build an HTML page
+
+// Use all of the collected employee data to build an HTML page
