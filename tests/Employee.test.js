aa209a8e4cdc0e0822eb05510786eed808ff1267
Christina Bohn <christinajbohn@gmail.com>
2022-01-16 23:09:33 -0800

    Adding pseudocode for unit tests into Employee.test.js

---
+++ tests/Employee.test.js
@@ -0,0 +1,40 @@
+const Employee = require('../lib/Employee');
+
+describe("Employee", () => {
+
+    // Methods
+    describe("Initialization / Constructor", () => {
+
+        // Testing of object properties after constructing a new object
+        it("should build with prop x when given y", () => {
+
+        })
+
+    });
+
+    describe("getName", () => {
+
+        // Arrange
+        // Create a new Employee object
+
+        // Act
+        // Call the getName method and store the result
+        
+        // Assert
+        // The result is equal to an expected value
+    
+    });
+
+    describe("getId", () => {
+
+        
+
+    });
+
+    describe("getEmail", () => {
+
+        
+
+    });
+
+});
