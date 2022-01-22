369bde119042b3d1b3376b57a2e0132a9ceac00d
Christina Bohn <christinajbohn@gmail.com>
2022-01-22 08:49:29 -0800

    Wrote unit tests for employee constructor.

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -2,9 +2,22 @@ const Employee = require('../lib/Employee');
 
 test('Can set name via constructor arguments', () => {
     const name  = 'Sam';
-    const employee = new Employee(name, 'sam@mail.com', 9);
+    const employee = new Employee(name, 9, 'sam@mail.com');
     expect(employee.name).toBe(name);
 })
+
+test('Can set ID via constructor arguments', () => {
+    const id  = 9;
+    const employee = new Employee('Sam', id, 'sam@mail.com');
+    expect(employee.id).toBe(id);
+})
+
+test('Can set email via constructor arguments', () => {
+    const email  = 'sam@mail.com';
+    const employee = new Employee('Sam', 9, email);
+    expect(employee.email).toBe(email);
+})
+
 
 // describe("Employee", () => {
 
