74824b3694970ecfafc5b7b2af8986d70401f208
Christina Bohn <christinajbohn@gmail.com>
2022-01-22 20:08:01 -0800

    Template html and css files deleted, fixed html.push functions in page-template.js to include employee.getRole().

--- src/page-template.js
+++ src/page-template.js
@@ -54,14 +54,14 @@ const generateTeam = team => {
 
     const html  = [];
 
-    html.push( team.filter( employee => getRole() === 'Manager')
-        .map( manager=> generateManager( manager )));
+    html.push( team.filter( employee => employee.getRole() === 'Manager')
+        .map( manager => generateManager( manager )));
     
-    html.push( team.filter( employee => getRole() === 'Engineer')
-        .map( engineer=> generateEngineer( engineer )).join(''));
+    html.push( team.filter( employee => employee.getRole() === 'Engineer')
+        .map( engineer => generateEngineer( engineer )).join(''));
 
-    html.push( team.filter( employee => getRole() === 'Intern')
-        .map( intern=> generateIntern( intern )).join(''));
+    html.push( team.filter( employee => employee.getRole() === 'Intern')
+        .map( intern => generateIntern( intern )).join(''));
 
     return html.join('')
 }
